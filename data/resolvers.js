343109193e3decbbcd19dd8071316e249a820156
Johan Steenkamp <johan.steenkamp@gmail.com>
2018-05-08 16:33:34 +1200

    Update GraphQL code

--- data/resolvers.js
+++ data/resolvers.js
@@ -3,16 +3,29 @@ import { Author, View, FortuneCookie } from './connectors';
 
 const pubsub = new PubSub();
 
-const channels = [
-  {
-    id: 1,
-    name: 'soccer',
-  },
-  {
-    id: 2,
-    name: 'baseball',
-  },
-];
+const channels = [{
+  id: '1',
+  name: 'soccer',
+  messages: [{
+    id: '1',
+    text: 'soccer is football',
+  }, {
+    id: '2',
+    text: 'hello soccer world cup',
+  }]
+}, {
+  id: '2',
+  name: 'baseball',
+  messages: [{
+    id: '3',
+    text: 'baseball is life',
+  }, {
+    id: '4',
+    text: 'hello baseball world series',
+  }]
+}];
+let nextId = 3;
+let nextMessageId = 5;
 
 const resolvers = {
   Subscription: {
@@ -25,9 +38,28 @@ const resolvers = {
       ),
     },
   },
+  Mutation: {
+    addChannel: (root, args) => {
+      const newChannel = { id: nextId++, name: args.name };
+      channels.push(newChannel);
+      return newChannel;
+    },
+    addMessage: (root, { message }) => {
+      const channel = channels.find(channel => channel.id === message.channelId);
+      if(!channel)
+        throw new Error("Channel does not exist");
+
+      const newMessage = { id: String(nextMessageId++), text: message.text };
+      channel.messages.push(newMessage);
+      return newMessage;
+    },
+  },
   Query: {
     channels: () => {
       return channels;
+    },
+    channel: (root, { id }) => {
+      return channels.find(channel => channel.id === id);
     },
     author(_, args) {
       return Author.find({ where: args });
--- data/schema.js
+++ data/schema.js
@@ -2,26 +2,6 @@ import { makeExecutableSchema } from 'graphql-tools';
 import resolvers from './resolvers';
 
 const typeDefs = `
-# todo - get subscriptions to work
-type Subscription {
-  messageAdded: [Channel]
-}
-
-type Channel {
-   id: ID!                # "!" denotes a required field
-   name: String
-}
-
-# This type specifies the entry points into our API. In this case
-# there is only one - "channels" - which returns a list of channels.
-
-type Query {
-  channels: [Channel]    # "[]" means this is a list of channels
-  author(firstName: String, lastName: String): Author
-  allAuthors: [Author]
-  getFortuneCookie: String @cacheControl(maxAge: 5)
-}
-
 type Author {
   id: Int
   firstName: String
@@ -35,6 +15,45 @@ type Post {
   text: String
   views: Int
   author: Author
+}
+
+type Message {
+  id: ID!
+  text: String
+}
+
+input MessageInput{
+  channelId: ID!
+  text: String
+}
+
+type Channel {
+   id: ID!                # "!" denotes a required field
+   name: String
+   messages: [Message]
+}
+
+# This type specifies the entry points into our API. In this case
+# there is only one - "channels" - which returns a list of channels.
+
+type Query {
+  channels: [Channel]    # "[]" means this is a list of channels
+  channel(id: ID!): Channel
+  author(firstName: String, lastName: String): Author
+  allAuthors: [Author]
+  getFortuneCookie: String @cacheControl(maxAge: 5)
+}
+
+# The mutation root type, used to define all mutations.
+type Mutation {
+  # A mutation to add a new channel to the list of channels
+  addChannel(name: String!): Channel
+  addMessage(message: MessageInput!): Message
+}
+
+# todo - get subscriptions to work
+type Subscription {
+  messageAdded: [Channel]
 }
 `;
 
